d62924be26090417bb9d9e43308e005b96ff2d8f
mella-kais <mallah.kais@gmail.com>
2020-05-11 15:00:55 +0100

    todo can be seen

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,31 +1,41 @@
 var toDolist = []
 
-function extracTodo (todoelement){
-	return todoelement.map(function(){
-		return $('<div class="todo-holder"><button class="todo-done">v</button><div class="theTodo">'+ todoelement.message +'</div><button class="todo-deleted">x</button></div>')
+function transformToDoList (todoelement){
+	return todoelement.map(function(todo){
+		return $('<div class="todo-holder"><button class="todo-done">v</button><div class="theTodo">'+ todo.message +'</div><button class="todo-deleted">x</button></div>')
 	})
-}
-
-function addTodo () {
-	var newtoDo = {
-	message : $('#todo-value').val(),
-	time_created : new Date
-	}
-	toDolist.push(newtoDo)
-	createTodo ()
 }
 
 function clearPrevious (elem) {
 	$(elem).html('');		
 }
 
+function addTodo () {
+	var newtoDo = {
+		user : "unknown",
+		message : $('#todo-value').val(),
+		}
+
+	toDolist.push(newtoDo)
+	$('#todo-value').value = '';
+	createTodo()
+	
+}
+
 function createTodo () {
 	clearPrevious ('.todo-second-section')
-	extracTodo(toDolist).forEach(function(todo){
+	transformToDoList(toDolist).forEach(function(todo){
 		$('.todo-second-section').prepend(todo)
 	});
 }
 
+
 $('#btn').on('click',function(){
 	addTodo()
-})s
+	
+})
+
+  
+$('.todo-deleted').on('click',function(){
+	$(this).attr('display' , 'none')
+})

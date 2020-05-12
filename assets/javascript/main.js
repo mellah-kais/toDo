dc04cd151b820321632d371146feba2bd14f9edd
mella-kais <mallah.kais@gmail.com>
2020-05-12 10:15:06 +0100

    morning second day

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,7 @@ var toDolist = []
 
 function transformToDoList (todoelement){
 	return todoelement.map(function(todo){
-		return $('<div class="todo-holder"><button class="todo-done">v</button><div class="theTodo">'+ todo.message +'</div><button class="todo-deleted">x</button></div>')
+		return $('<div class="todo-holder"><div class="todo-done"><i class="fas fa-check-double"></i></div><div class="theTodo">'+ todo.message +'</div><div class="delete" onclick="deleteTodo()" ><i class="far fa-window-close"></i></div></div>')
 	})
 }
 
@@ -10,18 +10,20 @@ function clearPrevious (elem) {
 	$(elem).html('');		
 }
 
+
 function addTodo () {
 	var newtoDo = {
 		user : "unknown",
 		message : $('#todo-value').val(),
 		}
-
-	toDolist.push(newtoDo)
-	$('#todo-value').value = '';
-	createTodo()
+		if($('#todo-value').val().length > 0) {
+			toDolist.push(newtoDo)
+			createTodo()
+		}
 	
-}
-
+		
+} 	
+	
 function createTodo () {
 	clearPrevious ('.todo-second-section')
 	transformToDoList(toDolist).forEach(function(todo){
@@ -32,10 +34,9 @@ function createTodo () {
 
 $('#btn').on('click',function(){
 	addTodo()
-	
-})
+});
 
-  
-$('.todo-deleted').on('click',function(){
-	$(this).attr('display' , 'none')
-})
+function deleteTodo() {
+		 $(this).parent().fadeOut('slow');
+};
+
